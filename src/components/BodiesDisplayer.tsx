3603673476dd39c31dbe6c96ba38d2117e27dc08
misat1505 <misat2002@gmail.com>
2024-12-12 22:52:15 +0100

    accordion for bodies

--- src/components/BodiesDisplayer.tsx
+++ src/components/BodiesDisplayer.tsx
@@ -1,5 +1,30 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import { useBodiesContext } from "@/context/BodiesContext";
+
 const BodiesDisplayer = () => {
-  return <div></div>;
+  const { bodies } = useBodiesContext();
+
+  return (
+    <div className="w-96 bg-slate-100">
+      <Accordion type="single" collapsible className="w-full">
+        {bodies.map((body, idx) => (
+          <AccordionItem value={`item-${idx}`} key={idx}>
+            <AccordionTrigger className="bg-slate-100 capitalize">
+              {body.name}
+            </AccordionTrigger>
+            <AccordionContent className="p-4">
+              Yes. It adheres to the WAI-ARIA design pattern.
+            </AccordionContent>
+          </AccordionItem>
+        ))}
+      </Accordion>
+    </div>
+  );
 };
 
 export default BodiesDisplayer;
