52285ebbdf7991cd242304f1725c3ab45c2c72cc
misat1505 <misat2002@gmail.com>
2024-12-13 12:58:49 +0100

    better accordion

--- src/components/BodiesDisplayer.tsx
+++ src/components/BodiesDisplayer.tsx
@@ -6,17 +6,12 @@ import {
 } from "@/components/ui/accordion";
 import { useBodiesContext } from "@/context/BodiesContext";
 import { Slider } from "./ui/slider";
+import { Body } from "@/types/Body";
+import { useEffect, useState } from "react";
+import { Button } from "./ui/button";
 
 const BodiesDisplayer = () => {
-  const { bodies, setBodies, initBodies } = useBodiesContext();
-
-  const handleMassChange = (name: string, newMass: number) => {
-    setBodies((prevBodies) =>
-      prevBodies.map((body) =>
-        body.name === name ? { ...body, mass: newMass } : body
-      )
-    );
-  };
+  const { bodies } = useBodiesContext();
 
   return (
     <div className="w-96 bg-slate-100">
@@ -27,25 +22,71 @@ const BodiesDisplayer = () => {
             <AccordionTrigger className="bg-slate-100 capitalize">
               {body.name}
             </AccordionTrigger>
-            <AccordionContent className="p-4">
-              <Slider
-                min={0.1}
-                step={0.1}
-                max={10}
-                defaultValue={[1]}
-                onValueChange={(value) => {
-                  handleMassChange(
-                    body.name,
-                    value[0] *
-                      initBodies.find((b) => b.name === body.name)!.mass
-                  );
-                }}
-              />
-            </AccordionContent>
+            <AccordionItemContent body={body} />
           </AccordionItem>
         ))}
       </Accordion>
     </div>
+  );
+};
+
+type AccordionItemContentProps = { body: Body };
+
+const AccordionItemContent = ({ body }: AccordionItemContentProps) => {
+  const [massMultiplier, setMassMultiplier] = useState(1);
+  const { setBodies, initBodies } = useBodiesContext();
+
+  const handleMassChange = (name: string, newMass: number) => {
+    setBodies((prevBodies) =>
+      prevBodies.map((body) =>
+        body.name === name ? { ...body, mass: newMass } : body
+      )
+    );
+  };
+
+  useEffect(() => {
+    handleMassChange(
+      body.name,
+      massMultiplier * initBodies.find((b) => b.name === body.name)!.mass
+    );
+  }, [massMultiplier]);
+
+  return (
+    <AccordionContent className="pb-0">
+      <div className="m-2 bg-slate-300/80 p-1 rounded-sm">
+        <h2 className="mb-4">
+          <span className="font-semibold">Mass:</span>{" "}
+          {massMultiplier.toFixed(1)}x
+        </h2>
+        <div className="flex items-center space-x-2">
+          <Button
+            className="font-semibold w-8 h-8 text-xl"
+            onClick={() =>
+              setMassMultiplier((prev) => Math.max(0.1, prev - 0.1))
+            }
+          >
+            -
+          </Button>
+          <Slider
+            min={0.1}
+            step={0.1}
+            max={10}
+            value={[massMultiplier]}
+            onValueChange={(value) => {
+              setMassMultiplier(value[0]);
+            }}
+          />
+          <Button
+            className="font-semibold w-8 h-8 text-xl"
+            onClick={() =>
+              setMassMultiplier((prev) => Math.min(10, prev + 0.1))
+            }
+          >
+            +
+          </Button>
+        </div>
+      </div>
+    </AccordionContent>
   );
 };
 
