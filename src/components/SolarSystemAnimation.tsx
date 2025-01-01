977f9b6cb283f3eae574c4b2eeb84c0615a26ddb
misat1505 <misat2002@gmail.com>
2025-01-01 21:02:12 +0100

    given descriptions for toggles

--- src/components/SolarSystemAnimation.tsx
+++ src/components/SolarSystemAnimation.tsx
@@ -177,48 +177,59 @@ const SolarSystem = () => {
   };
 
   return (
-    <div className="flex space-x-4 overflow-x-hidden justify-around items-center px-4 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-      <canvas
-        ref={canvasRef}
-        width={1200}
-        height={800}
-        onWheel={handleWheel}
-        onMouseDown={handleMouseDown}
-        onMouseMove={handleMouseMove}
-        onMouseUp={handleMouseUp}
-      />
-      <div>
-        <div className="w-[420px]">
-          <div className="border rounded-sm p-4">
-            <h2 className="font-bold text-lg mb-4 text-nowrap">
-              Time Multiplier ({timeMultiplier}x -{" "}
-              {(25 * timeMultiplier).toFixed(1)} days / second)
-            </h2>
-            <Slider
-              min={0.1}
-              step={0.1}
-              max={10}
-              defaultValue={[1]}
-              onValueChange={(value) => setTimeMultiplier(value[0])}
-            />
+    <div className="flex justify-center pt-20">
+      <div className="flex space-x-4 overflow-x-hidden justify-around items-start px-4">
+        <canvas
+          ref={canvasRef}
+          width={1200}
+          height={800}
+          onWheel={handleWheel}
+          onMouseDown={handleMouseDown}
+          onMouseMove={handleMouseMove}
+          onMouseUp={handleMouseUp}
+        />
+        <div className="h-[800px] overflow-y-auto overflow-x-hidden no-scrollbar">
+          <div className="w-[420px]">
+            <div className="border rounded-sm p-4">
+              <h2 className="font-bold text-lg text-nowrap">
+                Time Multiplier ({timeMultiplier}x -{" "}
+                {(25 * timeMultiplier).toFixed(1)} days / second)
+              </h2>
+              <p className="text-sm text-muted-foreground mb-4">
+                Control the speed of the animation. A higher multiplier makes
+                the simulation faster. If your PC struggles, the animation speed
+                can be throttled to reduce load.
+              </p>
+              <Slider
+                min={0.1}
+                step={0.1}
+                max={10}
+                defaultValue={[1]}
+                onValueChange={(value) => setTimeMultiplier(value[0])}
+              />
+            </div>
+            <div className="border rounded-sm my-4 p-4">
+              <h2 className="font-bold text-lg">
+                Traces Length (
+                {tracesLength > 0 ? `${tracesLength} days` : "hidden"})
+              </h2>
+              <p className="text-sm text-muted-foreground mb-4">
+                Adjust the length of the traces. The shorter the trace, the less
+                stress it puts on your computer's performance.
+              </p>
+              <Slider
+                min={0}
+                step={10}
+                max={1010}
+                defaultValue={[1010]}
+                onValueChange={(value) =>
+                  setTracesLength(value[0] > 1000 ? Infinity : value[0])
+                }
+              />
+            </div>
           </div>
-          <div className="border rounded-sm my-4 p-4">
-            <h2 className="font-bold text-lg mb-4">
-              Traces Length (
-              {tracesLength > 0 ? `${tracesLength} days` : "hidden"})
-            </h2>
-            <Slider
-              min={0}
-              step={10}
-              max={1010}
-              defaultValue={[1010]}
-              onValueChange={(value) =>
-                setTracesLength(value[0] > 1000 ? Infinity : value[0])
-              }
-            />
-          </div>
+          <BodiesDisplayer />
         </div>
-        <BodiesDisplayer />
       </div>
     </div>
   );
